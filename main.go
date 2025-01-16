674526fc44dad5a2390065a87ca26d9fb64c9a9e
cheildo <cheickzida@gmail.com>
2025-01-16 12:34:26 +0800

    Configure environment variables

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/cheildo/meme_coin_api/api/routes"
 	"github.com/cheildo/meme_coin_api/config"
 )
 
 func main() {
-	uri := "mongodb://localhost:27017"
+	uri := os.Getenv("MONGO_URI")
+	fmt.Println(uri)
 	dbName := "meme_coin_db"
 	collectionName := "meme_coins"
 
